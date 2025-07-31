6fb91e98d8480fd140cac0e87242781851e4e9a2
Nagum0 <xpteeth@gmail.com>
2025-07-31 10:13:27 +0200

    setup databases and tables for demo2

--- kubernetes/demo2/mysql-db/sql/data.sql
+++ kubernetes/demo2/mysql-db/sql/data.sql
@@ -1,21 +1,18 @@
-INSERT INTO Albums (artist, name, release_date) VALUES 
-('The Beatles', 'Abbey Road', '1969-09-26'),
+INSERT INTO Albums (artist, name, release_date) VALUES
 ('Pink Floyd', 'The Dark Side of the Moon', '1973-03-01'),
-('Led Zeppelin', 'Led Zeppelin IV', '1971-11-08'),
-('Michael Jackson', 'Thriller', '1982-11-30'),
-('Nirvana', 'Nevermind', '1991-09-24'),
-('Radiohead', 'OK Computer', '1997-05-21'),
-('Queen', 'A Night at the Opera', '1975-11-21'),
-('Bob Dylan', 'Highway 61 Revisited', '1965-08-30'),
 ('Fleetwood Mac', 'Rumours', '1977-02-04'),
+('Michael Jackson', 'Thriller', '1982-11-30'),
 ('Adele', '21', '2011-01-24'),
-('Kanye West', 'My Beautiful Dark Twisted Fantasy', '2010-11-22'),
-('Taylor Swift', '1989', '2014-10-27'),
-('The Rolling Stones', 'Sticky Fingers', '1971-04-23'),
-('David Bowie', 'The Rise and Fall of Ziggy Stardust', '1972-06-16'),
-('Bruce Springsteen', 'Born to Run', '1975-08-25'),
-('The Clash', 'London Calling', '1979-12-14'),
-('Metallica', 'Metallica (The Black Album)', '1991-08-12'),
-('Prince', 'Purple Rain', '1984-06-25'),
-('U2', 'The Joshua Tree', '1987-03-09'),
-('Madonna', 'Like a Virgin', '1984-11-12');
+('Kendrick Lamar', 'DAMN.', '2017-04-14');
+
+INSERT INTO Reviews (album_id, user, review) VALUES
+(1, 'MusicLover123', 'A timeless masterpiece, truly revolutionary sound!'),
+(1, 'ClassicRockFan', 'Still gives me chills every time I listen. Iconic album.'),
+(2, 'VinylCollector', 'Pure genius. Every song is a hit. Essential listening.'),
+(2, 'PopGuru', 'Fleetwood Mac at their absolute peak. So much emotion.'),
+(3, '80sKid', 'The King of Pop delivered big time. Billie Jean is still my jam!'),
+(3, 'NewGenListener', 'Even after all these years, Thriller holds up. Incredible production.'),
+(4, 'BalladQueen', 'Adele''s voice is just breathtaking. Someone Like You gets me every time.'),
+(4, 'ChartTopper', 'A phenomenal album that dominated the charts for a reason. So powerful.'),
+(5, 'HipHopHead', 'Kendrick''s storytelling is unmatched. DAMN. is a lyrical journey.'),
+(5, 'CritiquePro', 'Complex, thought-provoking, and sonically brilliant. A modern classic.');
--- kubernetes/demo2/mysql-db/sql/tables.sql
+++ kubernetes/demo2/mysql-db/sql/tables.sql
@@ -1,6 +1,14 @@
 CREATE TABLE Albums (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    artist VARCHAR(255),
-    name VARCHAR(255),
+    artist VARCHAR(255) UNIQUE,
+    name VARCHAR(255) UNIQUE,
     release_date DATE
+);
+
+CREATE TABLE Reviews (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    album_id INT NOT NULL,
+    user VARCHAR(255),
+    review VARCHAR(1024),
+    FOREIGN KEY (album_id) REFERENCES Albums(id)
 );
